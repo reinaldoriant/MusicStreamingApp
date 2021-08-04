36950e11eeef852f15c22c14054ae7531d282685
Reinaldo <reinaldo.riant@kompas.com>
2021-08-04 16:43:15 +0700

    Initial Commit

---
+++ MusicStreamingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicStreamingApp
+//
+//  Created by TI Digital on 04/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicStreamingApp/MusicStreamingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicStreamingAppApp.swift
+//  MusicStreamingApp
+//
+//  Created by TI Digital on 04/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicStreamingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
