a488ce5afcd66682e3622ad0fc2b63fdddbc2c39
Reinaldo <reinaldo.riant@kompas.com>
2021-08-05 19:01:31 +0700

    + add player function

--- MusicStreamingApp/View/MenuView.swift
+++ MusicStreamingApp/View/MenuView.swift
@@ -18,7 +18,7 @@ struct MenuView: View {
                 ScrollView(.horizontal, showsIndicators: false, content: {
                     LazyHStack{
                         ForEach(albums, id: \.id, content: { album in
-                            AlbumArt(album: album).onTapGesture {
+                            AlbumArt(album: album, isWithText: true).onTapGesture {
                                 currentAlbum = album
                             }
                         })
@@ -29,27 +29,30 @@ struct MenuView: View {
                                                                                              Song(name: "Song 1", time: "2.36"),
                                                                                              Song(name: "Song 1", time: "2.36"),
                                                                                              Song(name: "Song 1", time: "2.36")],id: \.id, content: { song in
-                                                                                                SongCell(song: song)
+                                                                                                SongCell(album: currentAlbum ?? albums.first!, song: song)
                                                                                     })
                    
                 }
-            }
+            }.navigationTitle("My Band Name")
         }
     }
 }
 
 struct AlbumArt: View {
     var album: AlbumDummyModel
+    var isWithText: Bool
     var body: some View {
         ZStack(alignment: .bottom,content:{
             Image(album.image)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 170, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            ZStack{
-                Blur(style:  .dark)
-                Text(album.name).foregroundColor(.white)
-            }.frame(height: 60,alignment: .center)
+            if isWithText == false {
+                ZStack{
+                    Blur(style:  .dark)
+                    Text(album.name).foregroundColor(.white)
+                }.frame(height: 60,alignment: .center)
+            }
            
         }).frame(width: 170, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).clipped().cornerRadius(20).shadow(radius: 10).padding(20)
         
@@ -57,16 +60,24 @@ struct AlbumArt: View {
 }
 
 struct SongCell: View {
+    var album : AlbumDummyModel
     var song: Song
     var body: some View {
         HStack{
-            ZStack{
-                Circle().frame(width: 50, height: 50, alignment: .center).foregroundColor(.blue)
-                Circle().frame(width: 20, height: 20, alignment: .center).foregroundColor(.white)
-            }
-            Text(song.name).bold()
-            Spacer()
-            Text(song.time)
+            NavigationLink(
+                destination: PlayerView(album: album, song: song),
+                label: {
+                    HStack{
+                        ZStack{
+                            Circle().frame(width: 50, height: 50, alignment: .center).foregroundColor(.blue)
+                            Circle().frame(width: 20, height: 20, alignment: .center).foregroundColor(.white)
+                        }
+                        Text(song.name).bold()
+                        Spacer()
+                        Text(song.time)
+                    }.padding(20)
+                }).buttonStyle(PlainButtonStyle())
+            
         }.padding(20)
     }
 }
---
+++ MusicStreamingApp/View/PlayerView.swift
@@ -0,0 +1,56 @@
+//
+//  PlayerView.swift
+//  MusicStreamingApp
+//
+//  Created by TI Digital on 05/08/21.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var album: AlbumDummyModel
+    var song: Song
+    
+    @State var isPlaying : Bool = false
+    
+    var body: some View {
+        ZStack {
+            Image(album.image).resizable().edgesIgnoringSafeArea(.all)
+            Blur(style: .dark).edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                AlbumArt(album: album, isWithText: false)
+                Text(song.name).font(.title).fontWeight(.light).foregroundColor(.white)
+                Spacer()
+                ZStack{
+                    Color.white.cornerRadius(20).shadow(radius: 10)
+                    HStack {
+                        Button(action: self.previous, label: {
+                            Image(systemName:"arrow.left.circle").resizable()
+                        }).frame(width: 50, height: 50, alignment: .center).foregroundColor(Color.black.opacity(0.2))
+                        Button(action: self.playPause, label: {
+                            Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill").resizable()
+                        }).frame(width: 70, height: 70, alignment: .center).padding()
+                        Button(action: self.next, label: {
+                            Image(systemName: "arrow.right.circle").resizable()
+                        }).frame(width: 50, height: 50, alignment: .center).foregroundColor(Color.black.opacity(0.2))
+                    }
+                    
+                }.edgesIgnoringSafeArea(.bottom).frame(height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+    
+    func playPause() {
+        self.isPlaying.toggle()
+    }
+    
+    func next() {
+        
+    }
+    
+    func previous() {
+        
+    }
+}
+
